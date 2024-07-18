312a18484c3bed67450dfe30989aa9366e954343
ratan <ratanchakma68@gmail.com>
2024-07-18 17:57:26 +0600

    some data type explain

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 var firstName = "ratan";
 console.log(firstName);
 var age = 45;
-console.log(age);
+// console.log(age);
+// built-in data type are : number, string, boolean, void, null, undefined
+// What is void data type?
+// Ans: যদি কোনো কিছু রিটার্ন না করে তাকে void data টাইপ বলে ।
+var studentRoll;
+var studentFirstName;
+var studentLastName;
+var isStudent;
+studentRoll = 45;
+studentFirstName = "Ratan";
+studentLastName = "Chakma";
+isStudent = true;
+var fullName = studentFirstName.concat(studentLastName);
+// console.log(fullName.split("  "),studentRoll,isStudent);
+// use of void data type
+function StudentName() {
+    // console.log("Hello I am Ratan Chakma");
+}
+// console.log(StudentName);
+// console.log(typeof(StudentName));
+console.log("=================== ==================== =================");
+// =================================================================
+// union data type  string|number
+var userId; // একের অধিক ডাটা টাইপ ব্যবহার করতে হলে এই ডাটা টাইপ ব্যবহার করতে হয়
+// console.log(userId= '45');
+// array data type
+var studentsName;
+var teams;
+var result = ["ratan", "kobita", 'lipika'];
+// console.log(teams = ["ratan","hello","lik"]);
+// console.log(result);
