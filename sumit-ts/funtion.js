d4552a780ad679737f5fbead1535bc00fb3107f0
ratan <your-email@example.com>
2024-10-02 23:20:27 +0600

    learn with sumit ts practice code added

---
+++ sumit-ts/funtion.js
@@ -0,0 +1,12 @@
+"use strict";
+// function myFunc (a:number, b:number, c?:number){
+// function myFunc (a:number, b:number, c:string = "ratan"){
+//     console.log(c);
+//     console.log(`The result is ${a} ${b} ${c}`);
+// }
+// // console.log(myFunc(10,30));
+// myFunc(10,30,"kamona");
+const myFunc = (a, b) => {
+    return a + b;
+};
+console.log(myFunc(10, 45));
---
+++ sumit-ts/funtion.ts
@@ -0,0 +1,14 @@
+// function myFunc (a:number, b:number, c?:number){
+// function myFunc (a:number, b:number, c:string = "ratan"){
+//     console.log(c);
+//     console.log(`The result is ${a} ${b} ${c}`);
+// }
+// // console.log(myFunc(10,30));
+// myFunc(10,30,"kamona");
+
+const myFunc = (a:number,b:number):number=>{
+    return a+b;
+}
+
+console.log(myFunc(10,45));
+
