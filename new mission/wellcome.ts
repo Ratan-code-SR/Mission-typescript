4d40b38e4ff021b29c049c6faa5aa97dd06ae8da
ratan chakma <ratanchakma68@gmail.com>
2025-03-18 13:14:13 +0600

    typescript data type have been added

--- new mission/wellcome.ts
+++ new mission/wellcome.ts
@@ -1,2 +1,30 @@
-let name = "Ratan chakma"
-console.log(name);
+// basic data type of type script
+// number
+let roll:Number = 23;
+// string
+let firstName:String = "Ratan"
+
+// boolean data type
+let isAdmin:Boolean = true;
+
+//  array data type 
+let list:number[] = [1,3,4,5]
+
+// tuple data type
+let collection: [string,number] = ["ratan", 3]
+
+// enum Data types 
+enum Color {
+    Red,
+    Green,
+    Blue
+}
+let c: Color = Color.Blue;
+
+// any types
+
+
+
+console.log({
+    roll, firstName, isAdmin
+});
